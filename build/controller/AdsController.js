9566f3f6635b23cc23be0f87a66ecd057f51fc8a
jrbigmon <junioterriaga0@gmail.com>
2022-09-12 15:29:16 -0300

    Adicionado ts-node-dev para desenvolvimento

--- build/controller/AdsController.js
+++
@@ -1,6 +0,0 @@
-const AdsController = {
-    getAds: (req, res) => {
-        res.json({ msg: 'Hello World!' });
-    }
-};
-export default AdsController;
--- build/router/AdsRouter.js
+++
@@ -1,8 +0,0 @@
-//express
-import express from 'express';
-const router = express.Router();
-//controller
-import AdsController from '../controller/AdsController.js';
-// routes
-router.get('/ads', AdsController.getAds);
-export default router;
