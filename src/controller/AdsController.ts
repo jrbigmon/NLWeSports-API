62000dcca0019510ed91f05b693f64f279e0a587
jrbigmon <junioterriaga0@gmail.com>
2022-09-15 16:06:57 -0300

    Importado prisma para usar como ORM

--- src/controller/AdsController.ts
+++ src/controller/AdsController.ts
@@ -1,3 +1,8 @@
+// Import prisma to uselly how ORM
+import { PrismaClient } from '@prisma/client'
+// instances prisma 
+const prisma = new PrismaClient()
+// controller to Ads
 const AdsController ={
     // get all ads
     getAds: (req: any, res: any) => {
