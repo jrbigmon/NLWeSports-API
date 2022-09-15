dc760dafc116f8429a86a3d7a9baa4ab0b5dafd9
jrbigmon <junioterriaga0@gmail.com>
2022-09-15 16:06:33 -0300

    Criado controller para games

---
+++ src/controller/GamesController.ts
@@ -0,0 +1,12 @@
+// Import prisma to uselly how ORM
+import { PrismaClient } from '@prisma/client'
+// instances prisma 
+const prisma = new PrismaClient()
+
+const GamesController = {
+    getGames: (req: any, res: any) => {
+
+    }
+}
+
+export default GamesController
