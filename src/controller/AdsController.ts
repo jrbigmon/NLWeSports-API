32931e9754377901a5bcffcda2a66b75c3980038
jrbigmon <junioterriaga0@gmail.com>
2022-09-15 16:52:47 -0300

    Criado listagem de todos os anúncios com innerjoin

--- src/controller/AdsController.ts
+++ src/controller/AdsController.ts
@@ -5,8 +5,22 @@ const prisma = new PrismaClient()
 // controller to Ads
 const AdsController ={
     // get all ads
-    getAds: (req: any, res: any) => {
-        return res.json({msg: 'Hello World!'})
+    getAds: async (req: any, res: any) => {
+        try {
+            const ads = await prisma.ad.findMany({
+                include: {
+                    game: {
+                        select: { 
+                            title: true 
+                        }
+                    }
+                }
+            })
+
+            return res.json(ads)
+        } catch (error) {
+            return res.status(500).json({message: 'Failed access database'})
+        }
     }
 }
 export default AdsController
