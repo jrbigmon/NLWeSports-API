b1e73a7679193405b776a2a248b62244fd2cda7f
jrbigmon <junioterriaga0@gmail.com>
2022-09-16 11:24:35 -0300

    BugFix para trativa de strings que geram números decimais

--- src/util/convertHoursStringToNumber.ts
+++ src/util/convertHoursStringToNumber.ts
@@ -1,5 +1,6 @@
 // 18:00 to 1080
 export function convertHoursStringToNumber(hoursString: string){
     const [hours, minutes] = hoursString.split(':', 2).map(i => parseInt(i))
-    return  (hours * 60) + (minutes * 60 / 100) 
+    
+    return  Math.round((hours * 60) + (minutes * 60 / 100)) 
 }
